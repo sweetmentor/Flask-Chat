51c1e1705adc93552132c55910027e5bb467e26b
Stephen Afolabi <stephenafolabi@gmail.com>
2018-05-24 16:08:00 +0000

    getting the message worked and redirected back

--- run.py
+++ run.py
@@ -3,19 +3,35 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask (__name__)
 
+# stores messages btw requests
+messages = []
+
 @app.route("/")
 def get_index():
-    return "Welcome to our chat app!"
-
+    return render_template("index.html")
+    
+    
+@app.route("/login")
+def login():
+    username = request.args.get('username')
+    return redirect(username) 
 
 
 @app.route("/<username>")
 def get_username(username):
-    return "Hello " + username
+    return render_template("chat.html", logged_in_as=username, all_the_messages=messages)
     
-@app.route("/<username>/<message>")
-def add_message(username, message):
-    return "<strong>{0}:</strong> {1}".format(username, message) 
+@app.route("/<username>/new", methods=["POST"])
+def add_message(username):
+    text = request.form['message']
+    message = {
+        'sender': username,
+        'body' : text
+    }
+    
+    messages.append(message)
+    return redirect(username) 
+    
 
     
     
