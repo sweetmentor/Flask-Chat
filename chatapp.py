c33554d60ac3224fb56ccefffbd73d7ec2fbb450
Stephen Afolabi <stephenafolabi@gmail.com>
2018-06-15 03:48:56 +0000

    first added functionality

--- chatapp.py
+++ chatapp.py
@@ -1,9 +1,9 @@
 import os
+from datetime import datetime
 from flask import Flask, redirect, render_template, request
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-# stores messages btw requests
 messages = []
 
 banned_words = [
@@ -13,32 +13,52 @@ banned_words = [
         'Cats'
     
     ]
-#Creating rooms
-# @app.route('rooms/add')
-# def add_room():
-#     roomname = request.form['roomname']
-#     rooms[roomname] = []
-#     return redirect(....)
-    
-@app.route("/")
-def get_index():
+
+
+def write_to_file(filename, data):
+    """Handle the process of writing data to a file"""
+    with open(filename, "a") as file:
+        file.writelines(data)
+
+
+def add_messages(username, message):
+    """Add messages to the `messages` text file"""
+    write_to_file("data/messages.txt", "({0}) {1} - {2}\n".format(
+            datetime.now().strftime("%H:%M:%S"),
+            username.title(),
+            message))
+
+
+def get_all_messages():
+    """Get all of the messages and separate them by a `br`"""
+    messages = []
+    with open("data/messages.txt", "r") as chat_messages:
+        messages = chat_messages.readlines()
+    return messages
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        write_to_file("data/users.txt", request.form["username"] + "\n")
+        return redirect(request.form["username"])
     return render_template("index.html")
-    
-    
-    
-    
-    
-@app.route("/login")
-def login():
-    username = request.args.get('username')
-    return redirect(username) 
 
 
-@app.route("/<username>")
-def get_username(username):
-    return render_template("chat.html", logged_in_as=username, all_the_messages=messages)
-  
-@app.route("/new", methods=["POST"])
+
+
+@app.route('/<username>')
+def user(username):
+    """Display chat messages"""
+    if request.method == "POST":
+        add_messages(username, request.form["message"] + "\n")
+    
+    
+    messages = get_all_messages()
+    return render_template("chat.html", username=username, chat_messages=messages)
+
+
+@app.route('/new')
 def add_message():
     username = request.form['username']
     text = request.form['message']
@@ -68,21 +88,16 @@ def add_message():
     
     messages.append(message)
     return redirect(username)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
-    
 
-    
-if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
-# app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)  
-    
+app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
+
+
+
+
+
+
+
+
+
+
+
